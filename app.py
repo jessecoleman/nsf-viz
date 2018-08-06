2fa76c175797b79f59ee9d70b31330cf35c4e456
Cole Chamberlin <jessec18@uw.edu>
2018-08-06 09:17:03 -0700

    added elasticsearch backend to replace mysql, improved frontend to render json data instead of csv

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template, request
 from gensim.models.word2vec import Word2Vec
-import multiprocessing
+import ruamel.yaml as yaml
+#import multiprocessing
 import logging
 import json
-import ruamel.yaml as yaml
 import pandas as pd
 from functools32 import lru_cache
 from flaskext.mysql import MySQL
 from collections import defaultdict
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search, MultiSearch, Document, Date, Keyword, Text, Index, connections
+from elasticsearch_dsl.query import MultiMatch
+
 
 app = Flask(__name__)
+es = Elasticsearch()
 mysql = MySQL()
 
 app.config["MYSQL_DATABASE_USER"] = "nsf"
@@ -18,6 +23,24 @@ app.config["MYSQL_DATABASE_DB"] = "nsf"
 app.config["MYSQL_DATABASE_HOST"] = "localhost"
 mysql.init_app(app)
 
+#nsf = Index('nsf')
+#nsf.settings(
+#        number_of_shards=8,
+#        number_of_replicas=2,
+#    )
+#
+#@nsf.document
+#class Grant(Document):
+#    title = Text()
+#    abstract = Text()
+#    amount = Text()
+#    date = Date()
+#    division = Keyword()
+#
+#    class Index:
+#        name = "nsf"
+#
+#Grant.init()
 
 @app.route("/rolling")
 def rolling():
@@ -44,9 +67,19 @@ def women_phil():
 #        return f.read()
 
 
-@app.route("/")
+@app.route("/index2")
 def main():
     return render_template("index.html")
+
+@app.route("/")
+def main2():
+    return render_template("index2.html")
+
+
+@app.route("/search2", methods=["POST"])
+def search2():
+    terms = frozenset(request.get_json())
+    return get_filtered(terms)
 
 @app.route("/search", methods=['GET', 'POST'])
 def search():
@@ -77,7 +110,6 @@ def search():
     #    p.join()
 
     #csv = "\n".join([p[0].recv() for p in pipe])
-    print(type(terms), type(div))
     csv = plot1(terms) + "\n" + plot2(terms, div)
 
     return csv
@@ -92,9 +124,32 @@ def suggestions():
             if t1 in word_vecs.vocab:
                 terms.append(t1)
 
-    related = word_vecs.most_similar(terms, [])
-    return ",".join([r[0] for r in related])
+    if len(terms) > 0:
+        related = word_vecs.most_similar(terms, [])
+        return ",".join([r[0] for r in related])
+    else:
+        return ""
 
+
+@app.route("/grants2", methods=['POST'])
+def grants2():
+
+    j = request.get_json()
+
+    if  j == "":
+        terms = keywords
+        with open("divisions.csv", "r") as f:
+            div = [l.strip() for l in f.readlines()]
+
+    else:
+        data = yaml.safe_load(j)
+        terms = data["terms"]
+        div = data["divisions"]
+        while len(div) < 2:
+            div.append("")
+
+    csv = "title,date,value,division\n" + fast_grants(terms, div)
+    return csv
 
 @app.route("/grants", methods=['POST'])
 def grants():
@@ -156,17 +211,68 @@ keywords = [
     #  'iplant',
 ]
 
-@app.route("/keywords")
-def get_keywords():
-    return json.dumps(keywords)
-
-@app.route("/divisions")
-def get_divisions():
-    return json.dumps([{
+@app.route("/defaults")
+def get_defaults():
+    return json.dumps({
+        "keywords": keywords,
+        "divisions": [{
             "value": d.strip(),#.lower().strip().replace(" ", "-"), 
-            "text": d.strip()
-        } for d in open("divisions.csv", "r").readlines()
-    ])
+            "text": d.strip(),
+            "default": True if i < 3 else False
+        } for i, d in enumerate(open("divisions.csv", "r").readlines())
+    ]})
+
+#@lru_cache(maxsize=40)
+def get_filtered(terms):
+
+    it = iter(terms)
+    
+    m = MultiMatch(query=next(it), fields=['title', 'abstract'], type="phrase")
+    # take union of all matching queries
+    l = len(terms) - 1
+    for t in range(l):
+        m = m | MultiMatch(query=next(it), fields=['title', 'abstract'], type="phrase")
+
+    s = Search(using=es)
+
+    s.aggs.bucket("per_year", "date_histogram", field="date", interval="year") \
+        .bucket("per_division", "terms", field="division", size=80) \
+        .metric("agg_grants", "value_count", field="date") \
+        .metric("agg_amount", "sum", field="amount")
+
+    total = s.execute().aggregations
+
+    s = Search(using=es).query(m)
+
+    s.aggs.bucket("per_year", "date_histogram", field="date", interval="year") \
+        .bucket("per_division", "terms", field="division", size=80) \
+        .metric("agg_grants", "value_count", field="date") \
+        .metric("agg_amount", "sum", field="amount")
+
+    matched = s.execute().aggregations
+
+    json_data = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+
+    for year in total.per_year.buckets:
+        y = year.key_as_string[:4]
+        for division in year.per_division.buckets:
+            json_data[y][division.key]["total_grants"] = division.agg_grants.value
+            json_data[y][division.key]["total_amount"] = division.agg_amount.value
+            json_data[y][division.key]["match_grants"] = 0
+            json_data[y][division.key]["match_amount"] = 0
+            json_data[y]["all"]["total_grants"] += division.agg_grants.value
+            json_data[y]["all"]["total_amount"] += division.agg_amount.value
+
+    for year in matched.per_year.buckets:
+        y = year.key_as_string[:4]
+        for division in year.per_division.buckets:
+            json_data[y][division.key]["match_grants"] = division.agg_grants.value
+            json_data[y][division.key]["match_amount"] = division.agg_amount.value
+            json_data[y]["all"]["match_grants"] += division.agg_grants.value
+            json_data[y]["all"]["match_amount"] += division.agg_amount.value
+
+    return json.dumps([{"year": year, "data": json_data[str(year)]} for year in range(2007, 2018)])
+
 
 @lru_cache(maxsize=20)
 def plot1(terms):
@@ -219,6 +325,22 @@ def plot2(terms, divisions):
     except:
         matching = total
     return pd.merge(total, matching, on='year', how='outer').to_csv(index=False)
+
+def fast_grants(terms, divisions):
+
+    it = iter(terms)
+
+    m = MultiMatch(query=next(it), fields=['title', 'abstract'], type="phrase")
+    # take union of all matching queries
+    l = len(terms) - 1
+    for t in range(l):
+        m = m | MultiMatch(query=next(it), fields=['title', 'abstract'], type="phrase")
+
+    print(divisions)
+    s = Search(using=es).query(m)
+
+    matched = ["\"{}\",{},{},{}".format(r.title, r.date, r.amount, r.division) for r in s.scan() if r.division in divisions]
+    return "\n".join(matched)
 
 #@lru_cache(maxsize=20)
 def grant_data(terms, divisions):
--- build_model.py
+++ build_model.py
@@ -1,6 +1,6 @@
 from gensim.models.word2vec import Word2Vec
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
 import mysql.connector
 
 db = mysql.connector.connect(host="localhost",
@@ -10,7 +10,7 @@ db = mysql.connector.connect(host="localhost",
 
 cursor = db.cursor()
 
-cursor.execute("select AwardTitle, AbstractNarration from Award")
+cursor.execute("select AwardTitle, AbstractNarration from Award limit 100")
 
 stop = set(stopwords.words('english'))
 stop.add(',')
@@ -20,11 +20,15 @@ result = cursor.fetchall()
 proc = []
 for r in result:
     proc.append([w.lower() 
-        for w in word_tokenize(r[0]) + word_tokenize(r[1]) 
+        for w in sent_tokenize(r[0].replace("<br/>", "\n")) +\
+                 sent_tokenize(r[1].replace("<br/>", "\n")) 
         if w.lower() not in stop])
 
+print(proc)
+exit()
+
 model = Word2Vec(proc, size=100, window=5, min_count=5, workers=4)
-model.save("nsf_w2v_model")
+model.save("nsf_w2v_model_2")
 similar = model.wv.most_similar(['data', 'science'], [])
 print(similar)
 
