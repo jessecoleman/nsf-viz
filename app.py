44fdbc0b10cd815e1ef88dde0e943eae1e67a282
Cole Chamberlin <jessec18@uw.edu>
2018-08-11 21:03:19 -0700

    removed jquery dependency, added resizing initial attempt

--- app.py
+++ app.py
@@ -104,6 +104,7 @@ word_vecs = Word2Vec.load("nsf_w2v_model").wv
 @app.route("/suggestions", methods=['POST'])
 def suggestions():
     terms = []
+    print(request.get_json());
     for t in request.get_json():
         for t1 in t.split():
             if t1 in word_vecs.vocab:
@@ -120,7 +121,6 @@ def suggestions():
 def grants():
 
     j = request.get_json()
-    print(j)
 
     if  j == "":
         terms = keywords
