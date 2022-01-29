01b72151c46623bc497940404657ecb61e366cc2
Cole Chamberlin <cchamberlin@apptek.com>
2022-01-29 14:21:17 -0800

    filtering now works with nested divisions, add react-flip-toolkit transitions to sidebar

--- frontend/src/app/divisions/Entry.tsx
+++ frontend/src/app/divisions/Entry.tsx
@@ -6,6 +6,7 @@ import { colorScales } from 'theme';
 import DivisionRow from './DivisionRow';
 import { KeyboardArrowDown, KeyboardArrowRight } from '@mui/icons-material';
 import { CheckboxState } from './DirectoryTableHead';
+import { Flipped } from 'react-flip-toolkit';
 
 const DirectoryRoot = styled(TreeItem)(({ theme }) => `
   color: ${theme.palette.text.secondary};
@@ -65,35 +66,39 @@ const DirectoryEntry = (props: DirectoryProps) => {
   } = props;
   
   return (
-    <Tooltip title={desc ?? ''}>
-      <DirectoryRoot
-        nodeId={nodeId}
-        expandIcon={<KeyboardArrowRight />}
-        collapseIcon={<KeyboardArrowDown />}
-        label={
-          <DivisionRow
-            checkable
-            key={nodeId}
-            dataKey={nodeId}
-            name={name}
-            onCheck={onCheck}
-            // onMouseOver={debouncedHighlight}
-            // onMouseOut={debouncedHighlight}
-            checked={checked}
-            aria-checked={checked}
-            cells={['count', 'amount'].map(field => ({
-              name: field,
-              value: props[field],
-              fill: colorScales[field](nodeId)
-            }))}
-            // tabIndex={-1}
-          />
-        }
-        {...other}
-      >
-        {children}
-      </DirectoryRoot>
-    </Tooltip>
+    <Flipped flipId={nodeId}>
+      <div>
+        <Tooltip title={desc ?? ''}>
+          <DirectoryRoot
+            nodeId={nodeId}
+            expandIcon={<KeyboardArrowRight />}
+            collapseIcon={<KeyboardArrowDown />}
+            label={
+              <DivisionRow
+                checkable
+                key={nodeId}
+                dataKey={nodeId}
+                name={name}
+                onCheck={onCheck}
+                // onMouseOver={debouncedHighlight}
+                // onMouseOut={debouncedHighlight}
+                checked={checked}
+                aria-checked={checked}
+                cells={['count', 'amount'].map(field => ({
+                  name: field,
+                  value: props[field],
+                  fill: colorScales[field](nodeId)
+                }))}
+                // tabIndex={-1}
+              />
+            }
+            {...other}
+          >
+            {children}
+          </DirectoryRoot>
+        </Tooltip>
+      </div>
+    </Flipped>
   );
 };
 
