253126b53bc94db7a1e25c573e007f1ae17c4ee3
hi-the-swan <jason.portenoy@gmail.com>
2022-02-10 10:02:57 -0800

    set base url to relative path

--- frontend/src/app/queryClient.ts
+++ frontend/src/app/queryClient.ts
@@ -2,11 +2,12 @@ import { QueryClient } from 'react-query';
 import Axios from 'axios';
 import { stringify } from 'query-string';
 
-if (process.env.NODE_ENV === 'development') {
-  Axios.defaults.baseURL = 'http://localhost:8888';
-} else {
-  Axios.defaults.baseURL = 'http://backend:8888';
-}
+// if (process.env.NODE_ENV === 'development') {
+//   Axios.defaults.baseURL = 'http://localhost:8888';
+// } else {
+//   Axios.defaults.baseURL = 'http://backend:8888';
+// }
+Axios.defaults.baseURL = './data';
 
 Axios.defaults.paramsSerializer = params => stringify(params);
 
