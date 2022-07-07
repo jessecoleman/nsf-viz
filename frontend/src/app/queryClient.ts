536acbd429a7930aec774333a6526a3d0b305340
Cole Chamberlin <cchamberlin@apptek.com>
2022-07-07 16:07:02 -0700

    bump some dependency versions and cast Route to any

--- frontend/src/app/queryClient.ts
+++ frontend/src/app/queryClient.ts
@@ -1,13 +1,13 @@
-import { QueryClient } from "react-query";
-import Axios from "axios";
-import { stringify } from "query-string";
+import { QueryClient } from 'react-query';
+import Axios from 'axios';
+import { stringify } from 'query-string';
 
 if (process.env.REACT_APP_LOCAL) {
-  Axios.defaults.baseURL = "http://localhost:8888";
+  Axios.defaults.baseURL = 'http://localhost:8888';
 } else if (process.env.CIP_API) {
-  Axios.defaults.baseURL = "https://grantexplorer-api.apps.cip.uw.edu/data";
+  Axios.defaults.baseURL = 'https://grantexplorer-api.apps.cip.uw.edu/data';
 } else {
-  Axios.defaults.baseURL = "/data";
+  Axios.defaults.baseURL = '/data';
 }
 
 Axios.defaults.paramsSerializer = (params) => stringify(params);
